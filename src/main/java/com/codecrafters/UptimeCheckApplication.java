4e7018ff0fa27daa7cce0d41131064ab6fe0df83
Fabian Dietenberger <piano.fabian@t-online.de>
2015-10-21 18:44:12 +0200

    Init repository

---
+++ src/main/java/com/codecrafters/UptimeCheckApplication.java
@@ -0,0 +1,12 @@
+package com.codecrafters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptimeCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UptimeCheckApplication.class, args);
+    }
+}
