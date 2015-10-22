5740e7afd1a3c4a07bf0a87abd696ed0f85771ea
Fabian Dietenberger <piano.fabian@t-online.de>
2015-10-22 20:13:13 +0200

    Add builder to slack message

--- src/main/java/com/codecrafters/konrad/SlackMessage.java
+++ src/main/java/com/codecrafters/konrad/SlackMessage.java
@@ -1,5 +1,7 @@
 package com.codecrafters.konrad;
 
+import java.util.Map;
+
 /**
  * This class is used to represent a message for the slack webhook api.
  *
@@ -12,11 +14,7 @@ public class SlackMessage {
     private String text;
     private String username;
 
-    public SlackMessage() {
-    }
-
-    public SlackMessage(final String text) {
-        this.text = text;
+    private SlackMessage() {
     }
 
     public String getText() {
@@ -33,5 +31,41 @@ public class SlackMessage {
 
     public void setUsername(final String username) {
         this.username = username;
+    }
+
+    /**
+     * Get an empty message object.
+     *
+     * @return a new message object
+     */
+    public static SlackMessage build(final String text) {
+        final SlackMessage message = new SlackMessage();
+        message.setUsername("konrad");
+        message.setText(text);
+        return message;
+    }
+
+    /**
+     * Get a pre-build message object.
+     *
+     * @param urlStatuses to status mapping to each url
+     * @return a new message object
+     */
+    public static SlackMessage build(final Map<String, Boolean> urlStatuses) {
+        final SlackMessage message = new SlackMessage();
+        message.setUsername("konrad");
+
+        final StringBuilder urlResultListMessage = new StringBuilder();
+        urlStatuses.forEach((url, isOk) -> urlResultListMessage.append(getMessageToUrlStatus(url, isOk)));
+        message.setText(urlResultListMessage.toString());
+
+        return message;
+    }
+
+    private static String getMessageToUrlStatus(final String url, final boolean isOk) {
+        if (isOk) {
+            return ":white_check_mark: " + url + "\n";
+        }
+        return ":no_entry: " + url + "\n";
     }
 }
