0e89096d401dd402cb4e15db61eb4daf453e7ca0
Fabian Dietenberger <piano.fabian@t-online.de>
2015-11-14 18:35:46 +0100

    Add slack class to send messages to slack

---
+++ src/main/java/com/codecrafters/konrad/slack/Slack.java
@@ -0,0 +1,34 @@
+package com.codecrafters.konrad.slack;
+
+import com.codecrafters.konrad.KonradProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * This class is used to send messages to slack.
+ *
+ * @author Fabian Dietenberger
+ */
+@Component
+public class Slack {
+
+    private final RestTemplate restTemplate;
+
+    private final KonradProperties properties;
+
+    @Autowired
+    public Slack(final RestTemplate restTemplate, final KonradProperties properties) {
+        this.restTemplate = restTemplate;
+        this.properties = properties;
+    }
+
+    public void send(final SlackMessage message) {
+        restTemplate.postForEntity(properties.getWebhookurl(), message, null);
+    }
+
+    public void send(final String text) {
+        final SlackMessage message = SlackMessage.builder().text(text).build();
+        send(message);
+    }
+}
