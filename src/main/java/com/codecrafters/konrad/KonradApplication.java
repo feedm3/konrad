2b54d3f5ecda8234b8e8c36652189d742902d1ba
Fabian Dietenberger <piano.fabian@t-online.de>
2016-01-12 21:50:20 +0100

    Create a nice message after startup

--- src/main/java/com/codecrafters/konrad/KonradApplication.java
+++ src/main/java/com/codecrafters/konrad/KonradApplication.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.TimeUnit;
+
 import static com.google.common.base.Preconditions.checkArgument;
 
 @SpringBootApplication
@@ -29,7 +31,7 @@ public class KonradApplication {
             checkArgument(properties.getUrls() != null, "URLs to check must not be null");
 
             try {
-                slack.send("konrad is up and running");
+                slack.send(createStartingMessage(properties));
             } catch (Exception ignored) {
                 logger.error("Slack Webhook URL not working. Plase check your webhook URL and restart konrad");
                 System.exit(0);
@@ -40,5 +42,49 @@ public class KonradApplication {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    /**
+     * Create a starting message which will be send to slack when the application has started.
+     *
+     * @param konradProperties the properties of the application to determine the message
+     * @return the message
+     */
+    private String createStartingMessage(final KonradProperties konradProperties) {
+        final StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("konrad is up and running. ");
+        stringBuilder.append("konrad will check ");
+        stringBuilder.append(konradProperties.getUrls().size());
+        stringBuilder.append(" urls every ");
+        stringBuilder.append(millisToString(konradProperties.getInterval()));
+        if (konradProperties.isReportOnlyWhenBrokenUrls()) {
+            stringBuilder.append(" but report to you only when a url is down.");
+        } else {
+            stringBuilder.append(" and message you the result. ");
+            stringBuilder.append("If you get too many messages you can set ");
+            stringBuilder.append("a property to only get messages when there are broken urls.");
+        }
+        stringBuilder.append(" konrad will also send you a report of all URLs every midnight. ");
+        stringBuilder.append("If you dont get a message at midnight konrad may be down! ");
+        stringBuilder.append("Feel free to join this project at github.com/feedm3/konrad");
+        return stringBuilder.toString();
+    }
+
+    /**
+     * Format milli seconds to a string.
+     *
+     * @param milliSeconds the milli seconds to format
+     * @return the formatted string. examples: 20 secs; 05:00 min; 1:30 min
+     */
+    private String millisToString(final long milliSeconds) {
+        long seconds = milliSeconds / 1000;
+        if (seconds > 60) {
+            return String.format("%02d:%02d min",
+                    TimeUnit.MILLISECONDS.toMinutes(milliSeconds),
+                    TimeUnit.MILLISECONDS.toSeconds(milliSeconds) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(milliSeconds))
+            );
+        }
+        return seconds + " secs";
     }
 }
